ee1d69ef11f14dcb1df101dfb1bba42c15d36639
unknown <damianfearon@hotmail.com>
2023-02-19 19:09:42 +0000

    add final readme

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const fs = require('fs');
 const licenses = [
   {name: 'Apache 2.0', value: 'apache-2.0'},
   {name: 'GNU GPLv3', value: 'gpl-3.0'},
-  {name: 'MIT', value: 'mit'}
-
-  ];
+  {name: 'MIT', value: 'mit'},
+  {name: 'BSD 3', value: 'bsd-3'},
+  {name: 'None', value: 'none'}
+];
 
 
 inquirer.prompt([
@@ -63,6 +64,7 @@ inquirer.prompt([
     installation: answers.installation,
     usage: answers.usage,
     license: answers.license,
+    badge: `![License: ${answers.license}](https://img.shields.io/badge/License-${answers.license}-brightgreen.svg)`,
     contributing: answers.contributing,
     tests: answers.tests,
     github: answers.github,
@@ -70,45 +72,50 @@ inquirer.prompt([
   };
 
   let licenseBadge;
-  if (readmeTemplate.license === 'MIT') {
-    licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  if (readmeTemplate.license === 'mit') {
+      licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (readmeTemplate.license === 'apache-2.0') {
+      licenseBadge = `[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (readmeTemplate.license === 'gpl-3.0') {
+      licenseBadge = `[![License: GPL 3.0](https://img.shields.io/badge/License-GPL%203.0-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (readmeTemplate.license === 'bsd-3') {
+      licenseBadge = `[![License: BSD 3-Clause](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
   } else {
-    licenseBadge = `[![License: ${readmeTemplate.license.toUpperCase()}](https://img.shields.io/badge/License-${readmeTemplate.license}-yellow.svg)](https://opensource.org/licenses/${readmeTemplate.license})`;
+      licenseBadge = `![License: None](https://img.shields.io/badge/License-None-red.svg)`;
   }
-
+  
   const readmeContent = `
-# ${readmeTemplate.title}
-
-## Description
-${readmeTemplate.description}
-
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
-
-## Installation
-${readmeTemplate.installation}
-
-## Usage
-${readmeTemplate.usage}
-
-## License
-This project is licensed under the ${readmeTemplate.license} license.
-
-## Contributing
-${readmeTemplate.contributing}
-
-## Tests
-${readmeTemplate.tests}
-
-## Questions
-If you have any questions, please contact ${readmeTemplate.github} at ${readmeTemplate.email}.
-
-`;
+  # ${readmeTemplate.title}
+  
+  ## Description
+  ${readmeTemplate.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${readmeTemplate.installation}
+  
+  ## Usage
+  ${readmeTemplate.usage}
+  
+  ## License
+  ${readmeTemplate.badge}
+  
+  ## Contributing
+  ${readmeTemplate.contributing}
+  
+  ## Tests
+  ${readmeTemplate.tests}
+  
+  ## Questions
+  If you have any questions, please contact ${readmeTemplate.github} at ${readmeTemplate.email}.
+  `;
 
   fs.writeFile('README.md', readmeContent, (err) => {
     if (err) throw err;
